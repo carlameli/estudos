4e459b025ebe5d6429c1ee78cb7b2e2ed480015c
carlameli <carla.pilots@gmail.com>
2021-09-09 16:16:25 -0300

    first commit

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80.5
+console.log( `A velocidade do seu carro é ${vel}km/h`)
+if(vel>60){ //Condição simples
+    console.log('Você untrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Europa'
+console.log(`Vivendo ${país}`)
+if(país == 'Brasil'){
+    console.log('Você é Brasileira!')
+} else {
+    console.log(' Você é Estrangeira!')
+}
